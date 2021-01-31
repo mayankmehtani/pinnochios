da74edaaaffcc53d76f8fab36f4df38e3b87a108
mayankmehtani <mayank.mehtani@gmail.com>
2021-01-31 11:54:45 +0530

    set validity messages with events instead of query selector

--- pizza/static/typescript/login.ts
+++ pizza/static/typescript/login.ts
@@ -2,11 +2,11 @@ const main = new Vue({
     delimiters : ['[[',']]'],
     el: '#main',
     methods: {
-        setUsernameValidity: function() {
-            document.querySelector('input[name=username]').setCustomValidity("");
+        setUsernameValidityMessage: function(event) {
+            event.target.setCustomValidity("");
         },
-        setPasswordValidity: function() {
-            document.querySelector('input[name=password]').setCustomValidity("");
+        setPasswordValidityMessage: function(event) {
+            event.target.setCustomValidity("");
         }
     },
     data: {
@@ -14,17 +14,19 @@ const main = new Vue({
     }
 })
 
-function checkLoginFields(): boolean {
+function checkLoginFields(event): boolean {
     let username:string = document.querySelector('input[name=username]').value.trim();
     let password:string = document.querySelector('input[name=password]').value;
 
     if (!Boolean(username)) {
         document.querySelector('input[name=username]').setCustomValidity("Username cannot be empty");
         document.querySelector('input[name=username]').reportValidity();
+        event.preventDefault();
     }
     else if (!Boolean(password)) {
         document.querySelector('input[name=password]').setCustomValidity("Password cannot be empty");
         document.querySelector('input[name=password]').reportValidity();
+        event.preventDefault();
     }
 
     return Boolean(username) && Boolean(password);
